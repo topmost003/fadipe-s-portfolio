4f699a7d787bc12d66b1d8c75b2fbb303517443d
topmost003 <topmostdabest@gmail.com>
2025-07-25 17:02:11 +0100

    sidebar

--- src/static/Header.jsx
+++ src/static/Header.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
 import img from "../assets/WhatsApp Image 2025-07-23 at 09.12.24_95073c82.jpg"
+import menu from "../assets/menu new.png"
+import arrow from "../assets/cancel.png"
+import Sidebar from "./Sidebar"
 
 const Header = () => {
+ 
+  const [toggle ,setToggle] = useState (false)
+
+  const SwitchToggle = ()=>{
+    setToggle(!toggle)
+  }
+
+
   return (
+    <div>
     <div  className=" flex justify-between bg-black text-white h-[80px]
      py-3  fixed  items-center border-b-amber-50 border-b-[0.5px]  w-full px-[150px] 
      
-     
-     max-md:justify-center max-md:px-0  max-md:h-[53px]  ">
+     max-md:   max-md:px-[50px]  max-md:h-[53px] max-md: ">
 
-        <a href="#home"><img src={img} alt="" className="w-[100px] h-[70px]  hover:-translate-1 max-md:h-[50px] max-md:w-[80px]" /></a>
+        <a href="#home"><img src={img} alt="" className="w-[100px] h-[70px]  
+        hover:-translate-1 max-md:h-[50px] max-md:w-[80px]" /></a>
+
 
         <div className="flex gap-15 max-md:hidden">
         <a href="#about" className="hover:text-[#FF7F50] ">About</a>
@@ -17,9 +31,24 @@ const Header = () => {
         <a href="#contact" className="hover:text-[#FF7F50]  ">Contact</a>
         </div>
 
+
+
+<section>
+        <div className=" w-[30px] min-md:hidden" onClick={SwitchToggle}> 
+      {toggle? <img src={arrow}alt="" /> : <img src={menu} alt="" /> }
+        </div>
+
+        <div >
+          {toggle && <Sidebar/>}
+        </div>
+</section>
+     
+       </div>
+
   
-  
-    </div>
+
+
+</div>
   )
 }
 
--- src/static/Sidebar.jsx
+++ src/static/Sidebar.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 const Sidebar = () => {
 
   return (
-    <div>
-        
-      
-    </div>
+        <div  className="">
+    
+          
+           <div className="grid  border-1 rounded-2xl w-[200px] h-[200px] p-3 font-medium text-2xl fixed right-4 top-[53px] bg-black text-white">
+
+            <a href="#about" className="hover:text-[#FF7F50] ">About</a>
+            <a href="#project" className="hover:text-[#FF7F50] ">Project</a>
+            <a href="#skills" className=" hover:text-[#FF7F50] ">Skills</a>
+            <a href="#contact" className="hover:text-[#FF7F50]  ">Contact</a>
+            </div>
+            
+        </div> 
   )
 }
 
