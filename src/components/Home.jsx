ea0fc3c2d5c71b75abc3b5be2dc653f61085232a
topmost003 <topmostdabest@gmail.com>
2025-07-25 10:31:54 +0100

    res

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className=' h-[100vh] bg-[#AAA08D] z-50 pt-20  text-black flex flex-col justify-center items-center cursor-cell max-md:text-center max-md:w-full max-md:h-[60vh] '>
 
 
-      <h1>Hi, i'm<span   className='font-bold text-[50px] px-2'>Abiodun Quadri Fadipe</span></h1>
+      <h1>Hi, i'm<span   className='font-bold text-[50px] px-2 max-md:text-[40px]'>Abiodun Quadri Fadipe</span></h1>
       
 
       <div className="flex gap-4 items-center max-md:w-[400px] max-md:justify-center ">
